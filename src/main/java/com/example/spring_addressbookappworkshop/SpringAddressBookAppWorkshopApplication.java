73d880c9c15fd310d425f08e13fc5470714f783b
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-10-13 08:24:55 +0530

    Added: Project setup

---
+++ src/main/java/com/example/spring_addressbookappworkshop/SpringAddressBookAppWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_addressbookappworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddressBookAppWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAddressBookAppWorkshopApplication.class, args);
+    }
+
+}
