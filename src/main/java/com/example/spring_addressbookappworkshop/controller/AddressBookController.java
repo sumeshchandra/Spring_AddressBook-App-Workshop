1ec341fb1e8deac350ed9c73353d8161aee3129f
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-10-13 09:27:31 +0530

    Added: CURL operation in controller

--- src/main/java/com/example/spring_addressbookappworkshop/controller/AddressBookController.java
+++ src/main/java/com/example/spring_addressbookappworkshop/controller/AddressBookController.java
@@ -1,6 +1,9 @@
 package com.example.spring_addressbookappworkshop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_addressbookappworkshop.dto.AddressBookDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Sumesh Jena
@@ -9,5 +12,48 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/addressBookService")
 public class AddressBookController {
+
+    /**
+     *
+     * @return ResponseEntity for getting data
+     */
+    @RequestMapping({"/get",  "/"})
+    public ResponseEntity<String> getAddressBookData() {
+        return new ResponseEntity<>("Get successful Data : ", HttpStatus.OK);
+    }
+    /**
+     *
+     * @return ResponseEntity for getting Id
+     */
+    @GetMapping("/get/{Id}")
+    public ResponseEntity<String> getAddressBookDataById(@PathVariable("Id") int Id) {
+        return new ResponseEntity<>("Get successful for id : " + Id, HttpStatus.OK);
+    }
+    /**
+     *
+     * @return ResponseEntity for getting data
+     */
+    @PostMapping("/createPost")
+    public ResponseEntity<String> createAddressBook(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>("Created new contact " + addressBookDTO, HttpStatus.OK);
+    }
+    /**
+     *
+     * @return ResponseEntity for getting updated data
+     */
+    @PutMapping("/updatePut")
+    public ResponseEntity<String> updateData(@RequestBody AddressBookDTO addressBookDTO) {
+        return new ResponseEntity<>("Updated Data successful :" + addressBookDTO, HttpStatus.OK);
+    }
+    /**
+     *
+     * @return ResponseEntity for deleting data
+     */
+    @DeleteMapping("/delete/{Id}")
+    public ResponseEntity<String> deleteDataById(@PathVariable("Id") int Id) {
+        return new ResponseEntity<>("deleted data for Id :" + Id, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/spring_addressbookappworkshop/dto/AddressBookDTO.java
+++ src/main/java/com/example/spring_addressbookappworkshop/dto/AddressBookDTO.java
@@ -1,10 +1,17 @@
 package com.example.spring_addressbookappworkshop.dto;
 
+import lombok.Data;
+
 /**
  * @author Sumesh Jena
  * @version 0.0.1
  * @since 13-10-2021
  */
 
+@Data
 public class AddressBookDTO {
+    public String firstName;
+    public String lastName;
+    public int pincode;
+
 }
